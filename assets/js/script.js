59ffbacfc4836a969ce6d256e3acf4829a2345f4
Aaron <aaronrobinett@gmail.com>
2022-01-07 00:06:33 -0800

    added the first time block and wrote setHourClasses function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,28 @@
 $(document).ready(function () {
     // uses moment.js to show current day
     $('#today').text(moment().format('dddd, MMMM Do'));
+
+    function setHourClasses() {
+
+        var currentHour = moment().hours();
+
+        // goes through time blocks to check if before or after current hour
+        $('.time-block').each(function () {
+            var blockHour = parseInt($(this).attr('id'));
+
+            if (blockHour < currentHour) {
+                $(this).addClass('past');
+            } else if (blockHour === currentHour) {
+                $(this).removeClass('past');
+                $(this).addClass('present');
+            } else {
+                $(this).removeClass('past');
+                $(this).removeClass('present');
+                $(this).addClass('future');
+            }
+        });
+    }
+
+    setHourClasses();
 });
 
